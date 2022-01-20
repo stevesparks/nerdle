baf6f793e8a6f4051cc23436ada184143ef64e3e
Steve Sparks <sparks@bignerdranch.com>
2022-01-20 16:45:39 -0500

    Converts to a local storage based index

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -12,12 +12,26 @@ export const isWinningWord = (word: string) => {
   return solution === word
 }
 
-export const getWordOfDay = () => {
+export const getIndexBasedOnDay = () => {
   // January 1, 2022 Game Epoch
   const epochMs = 1641013200000
   const now = Date.now()
   const msInDay = 86400000
-  const index = Math.floor((now - epochMs) / msInDay)
+
+  return Math.floor((now - epochMs) / msInDay)
+}
+
+export const getIndexBasedOnPuzzlesPlayed = () => {
+  return loadIndexFromLocalStorage()
+}
+
+export const getIndex = () => {
+  // return getIndexBasedOnDay()
+  return getIndexBasedOnPuzzlesPlayed()
+}
+
+export const getWordOfDay = () => {
+  const index = getIndex()
 
   return {
     solution: WORDS[index].toUpperCase(),
@@ -26,3 +40,15 @@ export const getWordOfDay = () => {
 }
 
 export const { solution, solutionIndex } = getWordOfDay()
+
+const gameIndexKey = 'gameIndex'
+
+export const saveIndexToLocalStorage = (index: number) => {
+  localStorage.setItem(gameIndexKey, index.toString())
+}
+
+export const loadIndexFromLocalStorage = () => {
+  const state = localStorage.getItem(gameIndexKey)
+
+  return parseInt(state ?? "0")
+}
